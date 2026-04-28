2922bf198008a4a7852c2ef9a06d4d0ad4065d22
Rog1veN <igor.pacievitch@gmail.com>
2026-04-27 22:42:47 -0400

    ajuste routes

--- projeto/projetoReact/src/routes.js
+++ projeto/projetoReact/src/routes.js
@@ -1,17 +1,17 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Cadastro from './pages/Cadastro';
 import Main from './pages/Main';
 
 function AppRoutes() {
   return (
-    <BrowserRouter>
+    <HashRouter> {/* Trocamos BrowserRouter por HashRouter */}
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/cadastro" element={<Cadastro />} />
         <Route path="/main" element={<Main />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
